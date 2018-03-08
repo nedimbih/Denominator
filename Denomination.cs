e0df282164f29288fad8303cdb86d7add17683c7
Nedim <nedimbih@hotmail.com>
2018-03-08 22:24:56 +0100

    Add files via upload

---
+++ Denomination.cs
@@ -0,0 +1,78 @@
+﻿using static System.Console;
+
+namespace Denominator
+{
+	class Denomination
+    {
+        // this array holds all off the BAM denominations, expressed in fenings (1BAM = 100 fening), to simplify division operations
+        static readonly int[] Denominations = { 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000 };
+        
+        int amount;     // user's entry
+
+        // this array will hold number of repetitions for certain denominations in a given amount 
+        // for example, in entry of 55,00BAM, 20BAM denomination can be contained once, twice or none
+        // since 20BAM denomination has index 8 (in Denominations array), the element repetition[8] can hold values 0, 1 or 2
+        int[] repetitions;         
+         
+        public Denomination(int amount)
+        {
+            repetitions = new int[Denominations.Length];
+            this.amount = amount;
+        }
+
+        internal void Analyze()
+        // this program could have been made without this method
+        // however this makes code in Main cleaner 
+        {
+            Analyze(amount, Denominations.Length - 1);
+        }
+
+        void Analyze(int amount, int index)  
+        {
+            int maxDenomination = FindMaxDenomination(amount, index);
+            int maxTimes = FindMaxRepetition(amount, maxDenomination);
+
+            for (int usedTimes = maxTimes; usedTimes >-1; usedTimes--)
+            {
+                repetitions[maxDenomination] = usedTimes;
+                int remainder = amount - usedTimes * Denominations[maxDenomination];
+                if (remainder == 0)
+                    PrintCombination();
+                else
+                    if (maxDenomination > 0)
+                        Analyze(remainder, maxDenomination - 1);
+            }
+        }
+
+        // returns number of max repetitions of a certain denomination in a given amount
+        static int FindMaxRepetition(int value, int index)
+        {
+            return value / Denominations[index];
+        }
+
+        // returns the index number (in Denominations array) at which the biggest possible denomination for a given amount is found
+        // for example, for amount of 15,00BAM biggest denomination is 10BAM, so method returns index of 10BAM in Denominations array, in this example it is 7
+        // this omits check for denomination bigger then the given amount
+        static int FindMaxDenomination(int value, int index)
+        {
+            for (int i = index; i > -1; i--)
+                if (value >= Denominations[i])
+                    return i;
+            return 0;
+        }
+
+        void PrintCombination()
+        {
+            bool somethingWritten = false;
+            for (int i = Denominations.Length - 1; i > -1; i--)
+                if (repetitions[i] > 0)
+                {
+                    if (somethingWritten)
+                        Write(" + ");
+                    Write($"{repetitions[i]} * {Denominations[i] / 100f:c} ");
+                    somethingWritten = true;
+                }
+			WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// This program has been coded only to show/verify algorithm
+// It has been coded for happy case scenario and it could be further improved with entry checks, cleaner code etc. which have been omitted due to the fact that that is not relevant (for showing algorithmic solution)
+
+// Note that calculations for amounts bigger than 5,00BAM may take a while to finish.
+// In such a case, program may seem as it have entered an endless loop, although it is working fine 
+
+using System;
+using static System.Console;
+
+namespace Denominator
+{
+	class Program
+    {
+        static int userInput,correctedInput;
+        static void Main()
+        {
+            bool again = true;
+            do
+            {
+                ObtainEntry();
+                var entry = new Denomination(correctedInput);
+
+                WriteLine($"\nYour entry in amount of {userInput / 100f:c} has been rounded to amount of {correctedInput / 100f:C}. \nThat amount can be achieved in following combinations:\n");
+
+                entry.Analyze();
+
+                WriteLine($"\nYour entry in amount of {userInput / 100f:c} has been rounded to amount of {correctedInput / 100f:C}. \nAll the possible combination have been listed above!");
+                WriteLine("\n\nWould you like to analyze another amount?\nPress y for new analysis, or any other key to quit!");
+
+                if (ReadLine() != "y") again = false;
+            } while (again);
+        }
+
+        private static void ObtainEntry()
+        {
+            Write("\nPlease enter the amount: ");
+
+            // Entry is multiplied with 100 so it can be expressed in fenings (1BAM=100fening), same as Denominations array 
+            userInput = (int)(Single.Parse(ReadLine()) * 100);
+            // Entry is rounded to value divisible by 5 (the smallest denomination)
+            correctedInput = CorrectEntry(ref userInput);
+        }
+
+        private static int CorrectEntry(ref int startingValue)
+        {
+            int remainder = startingValue % 5;
+            return remainder < 3 ? (startingValue - remainder) : (startingValue - remainder + 5);
+        }
+    }
+}
